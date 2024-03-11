d9a09f141b4c704f28dfdce0e62fd151ccec4cf0
Ajay02193 <ajay02193@gmail.com>
2024-03-11 15:04:51 +0530

    git commit -m

--- Inheritance/Abstraction/Interface/Packages/Main.java
+++ Inheritance/Abstraction/Interface/Packages/Main.java
@@ -12,21 +12,24 @@ public class Main implements IQ{
 //        bl.gearChange(1);
 //        bl.speedUp(10);
 //        bl.printState();
-//        System.out.println(bl.toString());
+//        System.out.println(bl);
 //        bl.Default();
-//
+
 //        Bike bk =new Bike();
 //        bk.gearChange(2);
 //        bk.speedUp(25);
 //        bk.printState();
-//        System.out.println(bk.toString());
+//        System.out.println(bk);
 //        bk.Default();
 
 //        test.fun();
 //        test.show();
 //        Interface.stat();
-          test.display();
+//        test.display();
 
-
+        byte a=10,b=20;
+        byte c;
+        c=(byte)(a+b);
+        System.out.println(c);
     }
 }
