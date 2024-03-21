00fbdb7719bbc3f56f3e36e9221573fa0dd07ce8
Ajay02193 <ajay02193@gmail.com>
2024-03-21 15:14:43 +0530

    git commit -m

--- Multithreading/M1/Main.java
+++ Multithreading/M1/Main.java
@@ -1,12 +1,15 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.*;
 public class Main {
     public static void main(String[] args) {
-
+        ExecutorService es= Executors.newFixedThreadPool(100);
         Lock lock = new ReentrantLock();
         Test t = new Test(lock);
         for(int i =0;i<100;i++){
             Thread th = new Thread(t);
-            th.start();
+            //th.start();
+            es.submit(th);
         }
     }
 }
